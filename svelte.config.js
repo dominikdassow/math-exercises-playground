1238ff3223fcf8eccb8537fd797bbe51f51738a2
Dominik Dassow <dominik@dassow-net.de>
2021-12-11 17:12:45 +0100

    Add postcss, tailwindcss, scss

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,26 @@ import preprocess from "svelte-preprocess"
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
-  preprocess: preprocess(),
+  preprocess: [
+    preprocess({
+      postcss: true,
+      scss: {
+        prependData: '@import "src/variables.scss";'
+      }
+    })
+  ],
   kit: {
     adapter: adapter(),
-    target: "#svelte"
+    target: "#svelte",
+    vite: {
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: '@import "src/variables.scss";'
+          }
+        }
+      }
+    }
   }
 }
 
